428fe1f2851bfdacc9e2a67f5f112a0b53456326
Jeevan-36 <reddyjeevan2006@gmail.com>
2026-01-25 17:12:11 +0530

    Fix MongoDB connection URI variable name in connectDB function

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,7 +3,7 @@ import {DB_NAME} from '../constants.js'
 
 export const connectDB=async()=>{
     try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log('MongoDB Connected...');
     } catch (error) {
         console.log("Error in Database connection",error);
